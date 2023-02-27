c0500e36616ed53ec6a69c6d4ef7a21b24ae9ecc
Peter Sidorov <kripns06@gmail.com>
2023-02-28 02:22:38 +1200

    feat/ implement register and login controllers

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,51 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import NotFoundError from '../utils/errors/not-found-error';
 import BadRequestError from '../utils/errors/bad-request-error';
+import ConflictError from '../utils/errors/conflict-error';
 import User from '../models/user';
+
+export function createUser(req, res, next) {
+  const { name, email, password } = req.body;
+  return bcrypt.hash(password, 10)
+    .then((hash) => User.create({ name, email, password: hash })
+      .then((user) => {
+        res.send({
+          name: user.name,
+          email: user.email,
+          _id: user._id,
+        });
+      }))
+    .catch((err) => {
+      if (err.code && err.code === 11000) {
+        next(new ConflictError('Пользователь с такой почтой уже зарегистрирован'));
+      } else if (err.name === 'ValidationError') {
+        next(new BadRequestError('Переданы некорректные данные'));
+      } else {
+        next(err);
+      }
+    });
+}
+
+export function login(req, res, next) {
+  const { NODE_ENV, JWT_SECRET } = process.env;
+  const { email, password } = req.body;
+  User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign(
+        { _id: user._id },
+        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+        { expiresIn: '7d' },
+      );
+      return res.send({
+        name: user.name,
+        email: user.email,
+        _id: user._id,
+        token,
+      });
+    })
+    .catch(next);
+}
 
 export function getCurrentUser(req, res, next) {
   User.findById(req.user._id)
