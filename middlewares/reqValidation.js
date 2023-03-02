a6ca479cc5926071c39a2ca44ba56caf1e48a354
Peter Sidorov <kripns06@gmail.com>
2023-03-02 19:42:55 +1200

    refactor/ add validation of name length in reqValidation

--- middlewares/reqValidation.js
+++ middlewares/reqValidation.js
@@ -3,7 +3,7 @@ import urlPattern from '../utils/urlPattern.js';
 
 export const validateRegister = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required(),
+    name: Joi.string().required().min(2).max(30),
     email: Joi.string().email().required(),
     password: Joi.string().required(),
   }),
